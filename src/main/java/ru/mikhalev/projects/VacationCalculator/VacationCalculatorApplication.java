c345a67cbe944e09ea23aa7f6754e09eb81800e5
Иван Михалев <un1soon@MacBook-Air-Ivan.local>
2023-03-09 10:12:05 +0400

    first commit

---
+++ src/main/java/ru/mikhalev/projects/VacationCalculator/VacationCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.mikhalev.projects.VacationCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mikhalev/projects/VacationCalculator/VacationCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikhalev.projects.VacationCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
